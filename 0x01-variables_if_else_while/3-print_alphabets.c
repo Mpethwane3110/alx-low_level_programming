a486b2a44ff548a20c5ec9f46d89fd643530f791
Mpethwane3110 <mpethwanenomfundo@gmail.com>
2023-03-17 11:55:31 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main -program that prints the alphabet in lowercase.
+  *
+  * Return: 0 always
+  */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar'\n');
+	return (0);
+}
