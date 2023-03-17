d790af81b970c5ab2a91c3302f340af28b025042
Mpethwane3110 <mpethwanenomfundo@gmail.com>
2023-03-16 19:45:58 -0700

    whitespaces

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
   */
 int main(void)
 {
-	write(2, "and that pice of art is useful \n"-Dora Korpar, 2015-10-19\n", 59);
-eturn (1);
-
+	write(2, "and that pice of art is useful\n"-Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
