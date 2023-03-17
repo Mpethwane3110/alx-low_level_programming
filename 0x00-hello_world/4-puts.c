7bb89d1685d28c9f0b12f7c8200db2492a8065bb
Mpethwane3110 <mpethwanenomfundo@gmail.com>
2023-03-16 18:00:14 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.>
+/**
+  * main - this is the main function
+  * Return: give 0
+  /*
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
