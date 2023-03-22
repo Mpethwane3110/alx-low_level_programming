366af1b8b889d12f01c667275d260916d0d7d060
Mpethwane3110 <mpethwanenomfundo@gmail.com>
2023-03-22 10:29:19 -0700

    task 4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,7 @@
-int _islower(int c)
+int _islower(int co)
 {
-  if (c >= 97 && c <= 122)
-     return (1);
-  else
-     return (0);
-
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
 }
