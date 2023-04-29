b51658c85edd388e43acca40f76bc6d4dc10386e
Mpethwane3110 <mpethwanenomfundo@gmail.com>
2023-04-29 09:01:54 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/**first - prints "You are beat!"
+  *execute a function
+  */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
