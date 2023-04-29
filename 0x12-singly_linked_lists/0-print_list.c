e05972b7340035030d02eaedaf7420121823fac0
Mpethwane3110 <mpethwanenomfundo@gmail.com>
2023-04-29 08:21:35 -0700

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,11 +13,12 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
-		s++;
 		if (!h->str)
 			printf("[0] (nil)\n");
+
+		else printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
 	}
 	return (s);
 }
