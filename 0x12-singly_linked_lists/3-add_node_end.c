1a794b34b83d8ac02e18d1560b2b9fc6de9014b0
Mpethwane3110 <mpethwanenomfundo@gmail.com>
2023-04-29 08:49:56 -0700

    3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,9 +20,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		len++;
 
 	new = malloc(sizeof(list_t));
-
 	if (!new)
-
 	return (NULL);
 
 	new->str = strdup(str);
@@ -32,7 +30,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 	{
 		*head = new;
+		return (new);
 	}
+	while (temp->next)
+		temp = temp->next;
 	temp->next = new;
 		return (new);
 }
