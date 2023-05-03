296e74834929e7d24e3691c504d974b49aba653c
Mpethwane3110 <mpethwanenomfundo@gmail.com>
2023-05-03 10:02:35 -0700

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,3 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node strucy
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	while(h)
+	{
+		putchar("%d",h->n);
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
 
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+size_t listint_len(const listint_t *h)
+
+size_t n = 0;
+{
+        while (h)
+        {
+                n++;
+                h = h->next;
+        }
+        return (n);
+}
+~     
