26060adc211ad15b759846e499ecef1477eaef2b
Mpethwane3110 <mpethwanenomfundo@gmail.com>
2023-05-17 05:27:39 -0700

    2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, len, w = 0;
 
 	if (filename == NULL)
-		 return (-1);
+		return (-1);
 
 	if (text_content != NULL)
 	{
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
-	if (fd == -1 ||w == -1)
+	if (fd == -1 || w == -1)
 		return (-1);
 	close(fd);
 
