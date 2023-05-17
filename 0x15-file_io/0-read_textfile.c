c0610d02095a656fcd264d22514fd8b69ecc1af1
Mpethwane3110 <mpethwanenomfundo@gmail.com>
2023-05-17 04:31:03 -0700

    1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,13 +16,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t fd;
 
 	fd = open(filename, O_RDONLY);
-        if (fd == -1)
-                return (0);
+	if (fd == -1)
+		return (0);
 
 	buf = malloc(sizeof(char) * letters);
 	t = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
-	free(buf);
 	close(fd);
+	free(buf);
 	return (w);
 }
