f43bbf1bc98590e0d19656cd670ce11bc4782865
Mpethwane3110 <mpethwanenomfundo@gmail.com>
2023-08-25 19:05:55 -0700

    lib

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,7 @@
+int _islower(int co)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: nothing, return void
+*/
+
+int main(void)
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+
+	else
+	{
+		n *= -1;
+		return (n);
+	}
+}
